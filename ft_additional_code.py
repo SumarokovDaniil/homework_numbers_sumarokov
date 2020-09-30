28417b88614ad4eeac6342fd0d4232ec2c42eb8f
SumarokovDaniil <44470355+SumarokovDaniil@users.noreply.github.com>
2020-09-30 14:02:21 +0300

    Update ft_additional_code.py

--- ft_additional_code.py
+++ ft_additional_code.py
@@ -1,30 +1,3 @@
-def ft_pow(number, s):
-    previous = number
-    if s > 0:
-        for i in range(s - 1):
-            number *= previous
-        return number
-    elif s == 0:
-        return 1
-    for i in range(number):
-        number /= previous
-    return number
-
-
-def ft_rev_bin_num(number):
-    temp = number
-    bin_number = 0
-    count_digits = 0
-    while temp > 0:
-        temp //= 10
-        count_digits += 1
-    for digit in range(count_digits):
-        bin_number += number % 10 * ft_pow(2, digit)
-        number //= 10
-    return bin_number
-
-
-
 def ft_bin_num(number):
     p = 1
     d = 0
@@ -59,7 +32,30 @@ def ft_straight_code(number):
 def ft_reverse_code(number):
     return ft_straight_code(number)[::-1]
 
+def ft_pow(number, s):
+    previous = number
+    if s > 0:
+        for i in range(s - 1):
+            number *= previous
+        return number
+    elif s == 0:
+        return 1
+    for i in range(number):
+        number /= previous
+    return number
 
+
+def ft_rev_bin_num(number):
+    temp = number
+    bin_number = 0
+    count_digits = 0
+    while temp > 0:
+        temp //= 10
+        count_digits += 1
+    for digit in range(count_digits):
+        bin_number += number % 10 * ft_pow(2, digit)
+        number //= 10
+    return bin_number
 
 def ft_additional_code(number):
     q = str(rev.ft_reverse_code(number))
